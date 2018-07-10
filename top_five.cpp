cf61e7c547e9e2a4f9438f881c2f42a7d27c05fd
Akshita Raju <rajuakshita22@gmail.com>
2018-07-11 00:02:06 +0530

    to find the top five students in a class

---
+++ top_five.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<climis>
+using namespace std;
+struct student{
+	char name;
+	int marks;
+};
+class tfive{
+	int maxsize
+	student *arr;
+	int current_size;
+public:
+	tfive()
+	{
+		maxsize=5;
+		arr=new student[maxsize];
+		current_size=0;
+	}
+	~tfive(){
+		delete arr;
+	}
+	student top(){
+		if(current_size!=0){
+			return arr[0];
+		}
+		else return INT_MIN;
+	}
+	void insertion(student s)
+	{
+		if(current_size<maxsize)
+		{
+			arr[current_size]=s;
+			int child=current_size;
+			int parent=(child-1)/2;
+			current_size++;
+			while(parent>=0 && arr[parent].marks>arr[child].marks)
+			{
+				swap(&arr[child],&arr[parent]);
+				child=parent;
+				parent=(child-1)/2;
+			}
+		}
+		else if(current_size=maxsize && s.marks>arr[0].marks)
+		{
+			arr[0]=s;
+			int a=0;
+			while((a*2 + 1) < (current_size-1))
+			{
+				int left  = 2*a+1;
+				int right = 2*a+2;
+				int smallest = a;
+				if(arr[left].marks < arr[a].marks)
+				{
+					smallest = left;
+				}
+				if(arr[right].marks < arr[smallest].marks){
+
+					smallest = right;
+				}
+				if(smallest==a){break;}
+				swap(&arr[smallest], arr[a]);
+				a = smallest;
+			}
+		}
+	}
+	void display(){
+		for(int j=0;j<5;j++){
+			cout<<arr[j].name;
+		}
+	}
+}top_five;
+int main(){
+	int number,mks;
+	char nm;
+	cin>>number;
+	for(int i=0;i<number;i++){
+		student m;
+		cin>>nm;
+		m.name=nm;
+		cin>>mks;
+		m.marks=mks;
+		top_five.insertion(m);
+	}
+   top_five.display();
+	return 0;	
+}
